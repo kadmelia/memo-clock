1a36095ac95cabeea0d643424b8af6daa5c7beee
Olivier <ovh@olivierbarou.fr>
2019-08-26 23:35:09 +0200

    feat(all): init project

---
+++ back/src/Entity/Game.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\GameRepository")
+ */
+class Game
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $time;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $moment;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTime(): ?int
+    {
+        return $this->time;
+    }
+
+    public function setTime(int $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getMoment(): ?\DateTimeInterface
+    {
+        return $this->moment;
+    }
+
+    public function setMoment(\DateTimeInterface $moment): self
+    {
+        $this->moment = $moment;
+
+        return $this;
+    }
+}
