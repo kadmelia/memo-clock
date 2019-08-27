1a3a3ad7dd1ccd8cb25aead621ce7fec3b408d62
Olivier <ovh@olivierbarou.fr>
2019-08-27 23:17:59 +0200

    feat(fback): enables API on Game entity

--- back/src/Entity/Game.php
+++ back/src/Entity/Game.php
@@ -3,9 +3,11 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\GameRepository")
+ * @ApiResource
  */
 class Game
 {
@@ -26,16 +28,30 @@ class Game
      */
     private $moment;
 
+    /**
+     * Get Game id
+     * @return int
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * Get Game time in second.
+     *
+     * @return int
+     */
     public function getTime(): ?int
     {
         return $this->time;
     }
 
+    /**
+     * Set Game time in second.
+     *
+     * @return Game
+     */
     public function setTime(int $time): self
     {
         $this->time = $time;
@@ -43,11 +59,21 @@ class Game
         return $this;
     }
 
+    /**
+     * Get Game day and hour when was play the game
+     *
+     * @return DateTimeInterface
+     */
     public function getMoment(): ?\DateTimeInterface
     {
         return $this->moment;
     }
 
+    /**
+     * Set Game day and hour when was play the game
+     *
+     * @return Game
+     */
     public function setMoment(\DateTimeInterface $moment): self
     {
         $this->moment = $moment;
